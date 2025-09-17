68d0515c6c45e28138337c931a15e8328f21d044
Горин Пётр <peter14102005@gmail.com>
2025-09-17 13:12:41 +0300

    Шаг 5 (Аскинема будет сделана позже. Сейчас траблы)

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from brain_games.cli import welcome_user
+from random import randint
+
+def main():
+    print('Welcome to the Brain Games!')
+    n = welcome_user()
+    if game_start(n) == 1:
+        win(n)
+
+def game_start(arg):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(3):
+        number = randint(0, 100)
+        correct = 'yes' if number % 2 == 0 else 'no'
+        print(f'Question: {number}')
+        answer = input('Your answer: ')
+        if answer == correct:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct}'.")
+            print(f"Let's try again, {arg}!")
+            return False
+    return True
+
+def win(arg):
+    return print(f'Congratulations, {arg}!')
+
+if __name__ == '__main__':
+    main()
